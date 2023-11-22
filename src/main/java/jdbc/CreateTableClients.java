f5be70f912bbe394956f9cf70212e10add75f204
tatyana <meridian.danilova@yandex.ru>
2023-11-22 22:59:02 +0300

    2. Подключение модуля javaFX

---
+++ src/main/java/jdbc/CreateTableClients.java
@@ -0,0 +1,12 @@
+package jdbc;
+
+import java.sql.Connection;
+
+
+public class CreateTableClients {
+    private static Connection connection;
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/jdbc/DBService.java
+++ src/main/java/jdbc/DBService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class DBService {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+
     private static final String USER = "postgres";
     private static final String PASSWORD = "root";
     public static final String DRIVER = "org.postgresql.Driver";
@@ -33,55 +34,20 @@ public class DBService {
             Statement statement = connection.createStatement();
             // обновление statement
             statement.executeUpdate(sql);
+            // создание таблицы в БД
+            String createTableQuery = "CREATE TABLE customers ("
+                    + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                    + "first_name VARCHAR(50) NOT NULL,"
+                    + "last_name VARCHAR(50) NOT NULL,"
+                    + "telephone VARCHAR(11)";
+            statement.executeUpdate(createTableQuery);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public int executeQuery(String query) throws SQLException {
-        //creates a statement object
-        Statement stmt = this.connection.createStatement();
 
-        //issues the query instruction
-        ResultSet rs = stmt.executeQuery(query);
-        int rowCount = 0;
-
-        //iterates through the result set and count number of results.
-        if (rs.next()) {
-            rowCount++;
-        }
-
-        stmt.close();
-        return rowCount;
-    }
-
-    public List<List<String>> executeQueryAndReturnResult(String query) throws SQLException {
-        Statement stmt = this.connection.createStatement();
-
-
-        ResultSet rs = stmt.executeQuery(query);
-
-
-        ResultSetMetaData rsmd = rs.getMetaData();
-        int numCol = rsmd.getColumnCount();
-        int rowCount = 0;
-
-        //iterates through the result set and saves the data returned by the query.
-        boolean outputHeader = false;
-
-        List<List<String>> result = new ArrayList<List<String>>();
-
-        while (rs.next()) {
-            List<String> record = new ArrayList<String>();
-            for (int i = 1; i <= numCol; ++i) {
-                record.add(rs.getString(i));
-            }
-            result.add(record);
-        }
-        stmt.close();
-        return result;
-    }
 }
 
 
